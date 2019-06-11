c440f75bc64b62f1f97605460f1eb421a245bf64
codemile <nick@reactgular.com>
2019-06-11 16:59:38 -0400

    adds npm packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
+import {ReactionsModule} from '../../library/reactions/src/reactions.module';
 import {DemoComponent} from './demo/demo.component';
 import {ReactionEditorComponent} from './reaction-editor/reaction-editor.component';
 
 @NgModule({
     imports: [
         BrowserModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        ReactionsModule
     ],
     declarations: [
         DemoComponent,
